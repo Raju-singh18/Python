0c72d012187c78cfaeec96005624593e8021d314
Raju Singh <rajusingh.mca26@gmail.com>
2026-08-11 18:27:37 +0530

    Break And Continue Added

---
+++ BreakAndContinue.py
@@ -0,0 +1,12 @@
+
+# ! Continue
+for value in range(10):
+    if value %3 == 0:
+        continue;
+    print(value);
+
+# ! break
+for value in range(10):
+    if value == 5:
+        break
+    print(value);
