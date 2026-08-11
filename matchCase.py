9cb22db561e0bce940772e8c9d0f8ac1226d2e55
Raju Singh <rajusingh.mca26@gmail.com>
2026-08-11 18:16:29 +0530

    matchCase Added

---
+++ matchCase.py
@@ -0,0 +1,18 @@
+
+num=int(input("Enter a number: "));
+
+match num:
+    case 1:
+        print("One")
+    case 2:
+        print("Two")
+    case 3:
+        print("Three")
+    case 4:
+        print("Four")
+    case 5:
+        print("Four")
+    case 5:
+        print("Five")
+    case _:
+        print("Enter values from 1 to 5");
