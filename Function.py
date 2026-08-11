15c1a492d12972199d123b365d642aedf478f43c
Raju Singh <rajusingh.mca26@gmail.com>
2026-08-11 16:53:48 +0530

    Function Added

---
+++ Function.py
@@ -0,0 +1,42 @@
+
+def rahul():
+    print("Hello Rahul..")
+    print("Kaise ho?")
+    print("Kya kar rahe ho")
+    print("Kuch khaye the?");
+
+def sayali():
+    print("Hello Sayali..!")
+    print("bye sayali");
+
+# rahul();  #function call
+# sayali();
+
+def fun1():
+    print("Hello Bye..!");
+
+# fun1();
+
+def add():
+    x=2
+    y=4
+    z=x+y
+    print(z);
+# add();
+
+def fun2():
+    x=2
+    y=6
+    z=x+y
+    return z;
+
+# print(fun2());
+
+def func3():
+    x=3
+    y=6
+    z=x+y;
+
+k=func3();
+print(k); 
+# None
